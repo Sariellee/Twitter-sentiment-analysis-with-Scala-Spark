9e1d2f65c89d3a7dab2da7f899ed57c54c524a3e
Oleg <and.olega@gmail.com>
2019-11-17 23:35:27 +0300

    Fscore for Train dataset

--- src/main/scala/LogisticRegression.scala
+++ src/main/scala/LogisticRegression.scala
@@ -1,10 +1,11 @@
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.LinearSVC
+import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature._
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.sql.SparkSession
 
-object LinearRegression {
+object LogisticRegression {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder
@@ -83,7 +84,7 @@ object LinearRegression {
       .setHandleInvalid("skip")
 
     // chosen model is Support Vector Classifier
-    val model = new LinearSVC().setMaxIter(100)
+    val model = new LogisticRegression().setMaxIter(100)
 
     // initialize the pipeline
     val pipeline = new Pipeline().setStages(Array(tokenizer, remover, cv, idf, labelIndexer, model))
@@ -94,13 +95,42 @@ object LinearRegression {
     // do the predictions on the test set
     val predictions = pipelineFit.transform(testData)
 
+    // get the prediction and label from df
+    val results = predictions.select("prediction", "label")
+
+    // results.show(20)
+    // predictions.show(20)
+
+    // Instantiate metrics object
+    var metrics = new MulticlassMetrics(results.rdd.map(row => {(row.getDouble(0), row.getDouble(1))}))
+
+    // Confusion matrix
+    println("Confusion matrix:")
+    println(metrics.confusionMatrix)
+
+    // Overall Statistics
+    val newaccuracy = metrics.accuracy
+    println("Summary Statistics")
+    println(s"Accuracy = $newaccuracy \n")
+
+    // Metrics class for train data
+    metrics = new MulticlassMetrics(pipelineFit.transform(trainData)
+      .select("prediction", "label").rdd.
+      map(row => {(row.getDouble(0), row.getDouble(1))}))
+
+    // Print Fscore for each class
+    val labels = metrics.labels
+    labels.foreach {l =>
+      println(s"F1-Score Train Data($l) = " + metrics.fMeasure(l))
+    }
+
     // show transformations and predictions first 100 test data entries
     predictions.show(100)
 
     // evaluate the accuracy
-    val evaluator = new BinaryClassificationEvaluator().setRawPredictionCol("rawPrediction")
-    val accuracy = evaluator.evaluate(predictions)
-    println(s"Accuracy = $accuracy")
+    // val evaluator = new BinaryClassificationEvaluator().setRawPredictionCol("rawPrediction")
+    // val accuracy = evaluator.evaluate(predictions)
+    // println(s"Accuracy = $accuracy")
 
     // save the model
     pipelineFit.write.overwrite().save("model")
