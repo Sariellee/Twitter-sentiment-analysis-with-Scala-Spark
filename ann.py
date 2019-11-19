7139c19c81cc8fd9cec902a87732f36cdacd00d4
Semen Kiselev <1semencs1@gmail.com>
2019-11-19 18:50:26 +0300

    + annotations + charts

--- ann.py
+++ ann.py
@@ -8,5 +8,4 @@ for i, row in enumerate(df.values):
     print('{}/{} '.format(i, len(df)), row[1], row[2])
     class_id = int(input())
     df.loc[i,'ours'] = class_id
-    break
 df.to_csv('out_'+name)
